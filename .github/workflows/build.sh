6c766d032d5cbe467712fd9f53a7886f5a4fb774
f3uvert <geradaividov@gmail.com>
2025-12-02 01:52:47 +0300

    final

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,7 +1,7 @@
 cp -rf ./tests/checkstyle.xml ./checkstyle.xml
 cp -rf ./tests/suppressions.xml ./suppressions.xml
-cp -rf ./tests/gateway/Dockerfile ./gateway/Dockerfile
-cp -rf ./tests/server/Dockerfile ./server/Dockerfile
+cp -rf ./tests/gateway/Dockerfile ./shareit-gateway/Dockerfile
+cp -rf ./tests/server/Dockerfile ./shareit-server/Dockerfile
 cp -rf ./tests/docker-compose.yml ./docker-compose.yml
 mvn enforcer:enforce -Denforcer.rules=requireProfileIdsExist -P check --no-transfer-progress &&
 mvn verify -P check,coverage --no-transfer-progress &&
