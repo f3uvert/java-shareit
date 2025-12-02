b730155ce56b1ddd8a42920b8524dd9c44a320b3
f3uvert <geradaividov@gmail.com>
2025-12-03 02:17:07 +0300

    final

--- gateway/src/main/java/ru/practicum/shareit/gateway/dto/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/dto/UserDto.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.gateway.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,10 @@ import lombok.NoArgsConstructor;
 public class UserDto {
     private Long id;
 
+    @NotBlank(message = "Name cannot be blank")
     private String name;
 
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
     private String email;
 }
