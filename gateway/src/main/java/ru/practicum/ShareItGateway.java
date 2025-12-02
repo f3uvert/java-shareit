3da5213d01a12e25c432f0297bf7cddc144bfd19
f3uvert <geradaividov@gmail.com>
2025-12-02 18:02:09 +0300

    final

--- gateway/src/main/java/ru/practicum/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/ShareItGateway.java
@@ -3,7 +3,7 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.practicum.shareit.gateway")
 public class ShareItGateway {
     public static void main(String[] args) {
         SpringApplication.run(ShareItGateway.class, args);
--- server/src/main/java/ru/practicum/ShareItServer.java
+++ server/src/main/java/ru/practicum/ShareItServer.java
@@ -3,7 +3,7 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.practicum.shareit.server")
 public class ShareItServer {
     public static void main(String[] args) {
         SpringApplication.run(ShareItServer.class, args);
