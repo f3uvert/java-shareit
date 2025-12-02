63f58288c083dab9dca7b0f76f172b94676f3c8e
f3uvert <geradaividov@gmail.com>
2025-12-02 04:03:14 +0300

    final

---
+++ .github/workflows/wait-for-postgres.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+echo "Waiting for PostgreSQL at $host..."
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "PostgreSQL is up - executing command"
+exec $cmd
